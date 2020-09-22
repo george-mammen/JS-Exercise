65a3a4794d6b80b138102bec81418c7a71817078
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-22 17:53:25 +0530

    Create Love-Calculator.js

---
+++ Love-Calculator.js
@@ -0,0 +1,7 @@
+prompt("What is your name?");
+prompt("What is your partner's name?");
+var loveScore= Math.random()*100;
+loveScore=Math.floor(loveScore)+1;
+if(loveScore===100){
+
+alert("Your Love Score is "+loveScore+"%");
