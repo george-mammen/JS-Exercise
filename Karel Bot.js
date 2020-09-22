ebeaa14f9441c1b568e5b4bfa695a9040777fe98
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-22 10:30:35 +0530

    Update Karel Bot.js

--- Karel Bot.js
+++ Karel Bot.js
@@ -1,1 +1,47 @@
-//
+// Moving the bot from one corner to the other 
+
+//Method 1
+function main(){
+    move4();
+    turnLeft()
+    move4()(); 
+ }
+  
+function move4(){
+  move();
+  move();
+  move();
+  move();
+}
+
+// Method 2
+function main(){
+   otherend();
+   otherend();
+   otherend(); 
+   otherend();
+}
+  
+function otherend(){
+  move();
+  turnLeft();
+  move();
+  turnRight();
+}
+
+//Beeper on Diagonal
+function main(){
+   beeperdiag();
+   beeperdiag();
+   beeperdiag(); 
+   beeperdiag();
+}
+
+function beeperdiag(){
+  move();
+  turnLeft();
+  move();
+  putBeeper();
+  turnRight();  
+  
+  //Beeper Chessboard
