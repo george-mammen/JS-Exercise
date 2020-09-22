3f1dd4efbea41ae1b84fd28b5cec506b31f3f01a
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-23 00:41:50 +0530

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,1 +1,20 @@
-
+var count=1;
+var n=prompt("Enter the number of terms to be generated :");
+function fizzbuzz(){
+  while(count <= n){
+    if(count %3===0 &&count % 5===0){
+      output.push("FizzBuzz");
+    }
+    else if(count %3===0){
+      output.push("Fizz");
+    }
+    else if(count%5===0){
+      output.push("Buzz");
+    }
+    else{
+      output.push(count);
+    }
+    console.log(output);
+}
+     
+    
