32e92b8db7fef308a473b98ed375b1bff819aff5
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-22 17:25:06 +0530

    Update BMI-Calculator.js

--- BMI-Calculator.js
+++ BMI-Calculator.js
@@ -1,1 +1,7 @@
-
+var weight= prompt("Enter your weight in kilograms");
+var height= prompt("Enter your height in metres");
+function bmiCalculator(weight,height){
+  var bmi=weight/(height*height);
+  return Math.round(bmi);
+}
+alert(bmi);
