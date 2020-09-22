c471ba2bc9374db557f43447fa12de269ed2ba12
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-22 18:17:27 +0530

    Update Love-Calculator.js

--- Love-Calculator.js
+++ Love-Calculator.js
@@ -1,6 +1,20 @@
+
 prompt("What is your name?");
 prompt("What is your partner's name?");
 var loveScore= Math.random()*100;
 loveScore=Math.floor(loveScore)+1;
 alert("Your Love Score is "+loveScore+"%");
+if(loveScore >=70){
+  alert("You love each other like Romeo and Juliet");
+}
+else if(loveScore >=30 && loveScore<70){
+  alert("You love needs to improve");
+}
+else{
+  alert("You love each other ?");
+}
+
+
+  
+  
 
