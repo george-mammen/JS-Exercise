e1d688f5c649c857517e708ca2e138c2364253f6
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-24 22:13:13 +0530

    Update Statements-in-js.js

--- Statements-in-js.js
+++ Statements-in-js.js
@@ -19,3 +19,22 @@ document.querySelector("#list a");//Combination of id and Tag
 
 //For array specify order else first instance will be returned
 
+//adding & removing class to element
+
+document.querySelector("tag").classList.add("To be added class");
+
+document.querySelector("tag").classList.remove(" To be Removed class");
+
+
+// do and undo any action
+
+document.querySelector("tag").classList.toggle("To be added class");
+
+//text manipulation
+document.querySelector("tag").innerHTML="<em>What you want it to change as</em>";
+
+//Attribute Manipulation
+document.querySelector("tag").getAttribute("attribute");
+
+document.querySelector("tag").setAttribute("attribute","change to attribute");
+
