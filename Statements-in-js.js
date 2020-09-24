17163c3996d2404e97c1e49bebd3f85e3f6e505d
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-24 08:59:47 +0530

    Update Statements-in-js.js

--- Statements-in-js.js
+++ Statements-in-js.js
@@ -1,1 +1,21 @@
+// Call Element by tag with the item number for lists followed by attribute
+
+document.getElementsByTagName("tag")[item number];//Return is an array.
+
+document.getElementsByClassName("class")[item number];//Return is an array.
+
+document.getElementById("id"); //Return is one element since Id is independant
+
+document.getElementById("id").innerHTML="What you want it to change as";
+
+
+document.querySelector(""); //Call any type of input
+
+document.querySelector("li a");//Combination of Tag and Tag
+
+document.querySelector(".item a");//Combination of class and Tag
+
+document.querySelector("#list a");//Combination of id and Tag
+
+//For array specify order else first instance will be returned
 
