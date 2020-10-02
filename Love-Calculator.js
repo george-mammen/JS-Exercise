260313cccd7a7730ccb2bf261573fab5f3950f83
naveenbalachandran <bnaveen360@gmail.com>
2020-10-02 20:55:03 +0530

    Added Comments to Love calculator.js
    Explained Math.random() and math.floor()

--- Love-Calculator.js
+++ Love-Calculator.js
@@ -1,8 +1,9 @@
 
 prompt("What is your name?");
 prompt("What is your partner's name?");
+/* Math.random returns a number between 0 and 1, multiplying that with 100 gets a decimal less than 100*/
 var loveScore= Math.random()*100;
-loveScore=Math.floor(loveScore)+1;
+loveScore=Math.floor(loveScore)+1;//to round decimal to integer
 alert("Your Love Score is "+loveScore+"%");
 if(loveScore >=70){
   alert("You love each other like Romeo and Juliet");
